c8e8d5ec03dd1edd10aa3774dc700bb6482d792a
vandana414 <vandanakudigrama.shenoy@sjsu.edu>
2019-05-01 20:43:21 -0700

    Fixing fetch profile

--- Quora/Kafka/app/db.js
+++ Quora/Kafka/app/db.js
@@ -247,34 +247,25 @@ db.bookmark = function(values, successCallback, failureCallback) {
     });
 };
 
-let fetchProfileById = function(email_id) {
-  return (
-    Profile.findOne(
-      {
+let fetchProfileById = function (email_id) {
+    console.log(email_id)
+    return Profile.findOne({
         email: email_id
-      },
-      "firstname lastname credentials photo"
-    )
-      .then(docs => {
-        if (docs == null) {
-          docs = {};
-          docs.firstname = "Quora";
-          docs.lastname = "User";
-          docs.credentials = "";
-          docs.photo = "http://localhost:3001/profile_uploads/default_profile.png";
+    }, 'firstname lastname credentials photo').then((docs) => {
+        if (docs == null)
+        {
+            docs = {}
+            docs.firstname = "Quora"
+            docs.lastname = "User"
+            docs.credentials = ""
+            docs.photo = "http://localhost:3001/profile_uploads/default_profile.png"
         }
-        return docs;
-      })
-      // .catch(err => {
-      //   console.log(err);
-      //   return {};
-      // })
-      .catch(error => {
-        failureCallback(error);
-        return;
-      })
-  );
-};
+        return docs
+    }).catch((err) => {
+        console.log(err)
+        return {}
+    })
+}
 
 db.getAnswersByQuestionId = function(q_id, successCallback, failureCallback) {
   Questions.findOne({
