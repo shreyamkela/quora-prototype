36b56641fce4da88f25d5eec8cc7c659fd9fc211
Ajay Chauhan <chauhanajayvikram1992@gmail.com>
2019-05-03 22:37:32 -0700

    Fixing createIndex issue

--- Quora/backend/database/connection.js
+++ Quora/backend/database/connection.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 //mongoose.Promise = global.Promise;
 let uri = "mongodb+srv://canvas_user:2407Rakhee%21@cluster0-jjkgt.mongodb.net/quoradb?poolSize=10?retryWrites=true";
 mongoose.Promise = global.Promise;
-mongoose.connect(uri, { useNewUrlParser: true, poolSize: 5 });
+mongoose.connect(uri, { useNewUrlParser: true, poolSize: 5,  useCreateIndex: true  });
 let con1 = mongoose.connection;
 con1.on("error", console.error.bind(console, "Connection error in mongoose (backend folder): "));
 con1.once("open", function () {
