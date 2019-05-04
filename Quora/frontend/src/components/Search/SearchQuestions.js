5b5cae9e49caedec8cf3325ec0b271a66fe6f720
Ajay Chauhan <chauhanajayvikram1992@gmail.com>
2019-05-04 14:33:19 -0700

    Searching a question

--- Quora/frontend/src/components/Search/SearchQuestions.js
+++ Quora/frontend/src/components/Search/SearchQuestions.js
@@ -6,28 +6,79 @@ import API from "../../utils/API";
 
 class SearchQuestions extends Component {
 
-    async handleSearch(key)  {
+    async handleFollow(key) {
+        //console.log(key)
         try {
-            let data = { email: cookie.load("cookie_user"), questionId: key }
+            let data = { email: cookie.load("cookie_user"), type: 2, topicId: key }
             let response = null;
-            response = await API.post("searchQuestion", data);
-            if (response.data.toLowerCase().includes(key)) {
-                message.warning("Success")
+            response = await API.get("questionsFollowed", { params: data });
+            if (response.data.toLowerCase().includes("already")) {
+                message.warning("Question already followed!")
             } else {
-                message.success("Not Found")
+                message.success("Question followed!")
             }
         } catch (error) {
             console.log(error);
-            message.error("Unable to search.")
+            message.error("Unable to follow question at the moment. Please refresh the page and try again.")
         }
-}
+
+    }
+
+
+    async handleUnfollow(key) {
+        //console.log(key)
+        try {
+            let data = { email: cookie.load("cookie_user"), topicId: key }
+            let response = null;
+            response = await API.post("questionsUnfollowed", data);
+            if (response.data.toLowerCase().includes("already")) {
+                message.warning("Question is not followed!")
+            } else {
+                message.success("Question Unfollowed!")
+            }
+        } catch (error) {
+            console.log(error);
+            message.error("Unable to unfollow question at the moment. Please refresh the page and try again.")
+        }
+
+    }
+
+    // handleTopicLinkClick = (key) => {
+    //     this.props.history.push({ // This is how we pass data from this component to a child component i.e searchQuestions, using the history.push.
+    //         // This will change the route, render new component, and also pass data into the component.
+    //         // Passed data can be accessed in the child component through this.props.history.location.state.
+    //         // To pass these props into the child component we have used <Route exact path="/main/questions/search" render={(props) => <SearchQuestions {...props} />} />
+    //         pathname: `/main/questions/${key.title}/questions`,
+    //         state: {
+    //             selectedQuestion: key
+    //         }
+    //     })
+    // }
+
+//     async handleSearch(key)  {
+//         try {
+//             let data = { email: cookie.load("cookie_user"), questionId: key }
+//             let response = null;
+//             response = await API.post("searchQuestion", data);
+//             if (response.data.toLowerCase().includes(key)) {
+//                 message.warning("Success")
+//             } else {
+//                 message.success("Not Found")
+//             }
+//         } catch (error) {
+//             console.log(error);
+//             message.error("Unable to search.")
+//         }
+// }
 
     render() {
         // To access the state passed into this component from parent through this.props.history.push - we use this.props.history.location.state
         console.log("SearchQuestions component:####### ", this.props.history.location.state);
         let searchResults = null;
-        if (this.props.history.location.state.searchResults !== undefined) {
-            searchResults = this.props.history.location.state.searchResults
+        if (this.props.history.location.state !== undefined) {
+            if (this.props.history.location.state.searchResults !== undefined) {
+                searchResults = this.props.history.location.state.searchResults
+            }
         }
         let displayedResults = null;
         if (searchResults === null || searchResults[0] === undefined) {
