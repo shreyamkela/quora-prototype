b52f6b3cfe603b0836368eebc84cec02bf59f719
Ajay Chauhan <chauhanajayvikram1992@gmail.com>
2019-05-03 19:55:25 -0700

    Search questions added

--- Quora/backend/routes/SearchQuestions.js
+++ Quora/backend/routes/SearchQuestions.js
@@ -1,11 +1,42 @@
 var express = require("express");
 var router = express.Router();
+const Model = require("../database/connection");
+
 
 // Route to GET questions according to the search term
-router.get("/", function(req, res) {
-  console.log("GET /searchQuestions");
-  console.log("Req: ", req.query);
-  res.status(200).send("SUCCESS - Search questions");
+
+router.get("/", function (req, res) {
+    console.log("GET /searchQuestions");
+    console.log("Req: ", req.query[0].toLowerCase());
+
+    let searchValue = req.query[0].toLowerCase();
+    let searchedQuestions = [];
+
+    Model.questions.find({}, (err, results) => {
+        if (err) {
+            console.log("Unable to fetch questions", err);
+            res.status(400).send("Unable to fetch questions!");
+        } else {
+            if (results) {
+                for (var key in results) {
+                    let currentTerm = results[key].title.toLowerCase();
+                    if (currentTerm.includes(searchValue)) {
+                        searchedQuestions.push(results[key]);
+                    }
+                }
+                if (searchedQuestions === []) {
+                    console.log("No questions found");
+                    res.status(400).send("No topics found");
+                } else {
+                    console.log("searchedQuestions: ", searchedQuestions);
+                    res.status(200).send(searchedQuestions);
+                }
+            } else {
+                console.log("No Questions found in database!", err);
+                res.status(400).send("No Questions found in database!");
+            }
+        }
+    });
 });
 
 module.exports = router;
--- Quora/frontend/src/components/Search/SearchQuestions.js
+++ Quora/frontend/src/components/Search/SearchQuestions.js
@@ -1,21 +1,70 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Card } from "antd"
+import { Button, Card, Row, Col, message } from "antd";
+import cookie from "react-cookies";
+import API from "../../utils/API";
 
 class SearchQuestions extends Component {
+
+
     render() {
         // To access the state passed into this component from parent through this.props.history.push - we use this.props.history.location.state
-        console.log("SearchQuestions component: ", this.props.history.location.state)
+        console.log("SearchQuestions component: ", this.props.history.location.state);
         let searchResults = null;
         if (this.props.history.location.state.searchResults !== undefined) {
             searchResults = this.props.history.location.state.searchResults
         }
-        return <div><Card>Search Questions:</Card></div>;
+        let displayedResults = null;
+        if (searchResults === null || searchResults[0] === undefined) {
+            displayedResults = <div style={{ textAlign: "center", fontSize: 15 }}>No questions found for this search.</div>;
+        } else {
+            displayedResults = searchResults.map(key => (
+                <div style={{ textAlign: "center" }}>
+                    <div className="card" style={{ width: "70%", height: 100, textAlign: "center" }}>
+                        <div className="card-body">
+                            <h5 className="card-title" style={{ fontSize: 15, marginLeft: 20, marginTop: 20 }}>
+                                {key.title}
+                            </h5>
+                            <br />
+                            <Row style={{ marginLeft: 205 }}>
+                                <Col span={6}>
+                                    <Button size="small" icon="check-circle" shape="round" onClick={() => this.handleFollow(key._id)} >
+                                        Follow
+                                    </Button>
+                                </Col>
+                                <Col span={6}>
+                                    <Button size="small" icon="close-circle" shape="round" onClick={() => this.handleUnfollow(key._id)}>
+                                        Unfollow
+                                    </Button>
+                                </Col>
+                            </Row>
+                        </div>
+                    </div>
+                    <br />
+                    <br />
+                </div>
+            ));
+        }
+
+        return (
+            <div>
+                <Card>
+                    <div>
+                        <font size="4">
+                            <b>Results:</b>
+                        </font>
+                        <br />
+                        <br />
+                        {displayedResults}
+                    </div>
+                </Card>
+            </div>
+        );
     }
 }
 
 //This method is provided by redux and it gives access to centeral store
 function mapStateToProps(state) {
-    return {};
+    return ;
 }
 export default connect(mapStateToProps)(SearchQuestions);
