4843b9bff22a3ccb54c4b31b2de801d301ce94fb
ShravaniPande <47123250+ShravaniPande@users.noreply.github.com>
2019-05-07 11:12:44 -0700

    minor fx

--- Quora/frontend/src/components/LandingPage/Sidebar.js
+++ Quora/frontend/src/components/LandingPage/Sidebar.js
@@ -104,7 +104,7 @@ class Sidebar extends Component {
         
         }catch(error){
             console.log(error.response);
-            message.error("Unable to show followed topics. Please refresh the page.");
+          //  message.error("Unable to notifications");
         }
   }
 
