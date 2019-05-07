ecbfbafe90ea7ba86c67367e126bd325f13f488e
vandana414 <vandanakudigrama.shenoy@sjsu.edu>
2019-05-07 04:39:32 +0000

    Fix cross domain cookie issue

--- Quora/backend/routes/Login.js
+++ Quora/backend/routes/Login.js
@@ -7,10 +7,10 @@ router.post('/', function (req, response) {
         if (err) {
             response.status(401).json({success: false,message:msg});
         } else {
-            response.cookie('cookie_user', req.body.email_id, { maxAge: 900000, httpOnly: false, path: '/' });
-            response.cookie('auth_token', msg.token, { httpOnly: false, path: '/' });
+            response.cookie('cookie_user', req.body.email_id, { domain: '52.14.67.136', maxAge: 900000, httpOnly: false, path: '/' });
+            response.cookie('auth_token', msg.token, { domain: '52.14.67.136', httpOnly: false, path: '/' });
             req.session.user = req.body.email_id;
-            response.status(200).json({success: true,message:"Login Success"});
+            response.status(200).json({success: true,message:req.body.email_id});
         }
     })
 });
